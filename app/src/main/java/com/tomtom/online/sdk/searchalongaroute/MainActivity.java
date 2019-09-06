4ed2c0ca2924d15227302fd26af3c2fcce52e007
Martin Migasiewicz <616250+martinm82@users.noreply.github.com>
2019-09-06 13:58:47 +0200

    add hints for student where to add code

--- app/src/main/java/com/tomtom/online/sdk/searchalongaroute/MainActivity.java
+++ app/src/main/java/com/tomtom/online/sdk/searchalongaroute/MainActivity.java
@@ -5,9 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+  /*
+  Add here the TomtomMap member
+  */
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    /*
+    Add here the obtaining of the MapFragment reference and
+    registering of the OnMapReadyCallback.
+    */
   }
+
+  /*
+  Add here the onMapReady callback
+  */
+
+  /*
+  Add here the onRequestPermissionResult
+  */
 }
